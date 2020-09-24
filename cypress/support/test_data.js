8a55fba11e6b0152c0e7482ce6c1762a3e66e03b
Tyler Dallinga <tyler.dallinga@gmail.com>
2020-09-24 07:55:53 -0600

    finished the second test, and improved first test

--- cypress/support/test_data.js
+++ cypress/support/test_data.js
@@ -8,5 +8,35 @@ module.exports = {
 
         },
     },
+    riders: {
+        service: {
+            rider1: "S",
+        },
+        first: {
+            rider1: "John",
+        },
+        last: {
+            rider1: "Doe",
+        },
+        phone: {
+            rider1: "5550123456",
+        },
+        riders: {
+            rider1: "2",
+        },
+        wheelchair: {
+            yes: "mousedown",
+            no: "",
+        },
+        note: {
+            rider1: "this is a note",
+        },
+        pickup: {
+            rider1: "Davis Dr at Hopson Rd Stop",
+        },
+        dropoff: {
+            rider1: "Starbucks Slater Road, Drh",
+        }
+    }
 
 }
